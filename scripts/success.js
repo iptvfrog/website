26dc575349c0e1bbb9903a4fdf776a5b76da8dfb
iptvfrog <iptvfrog@gmail.com>
2017-09-28 22:28:54 -0400

    Update success.js

--- scripts/success.js
+++ scripts/success.js
@@ -18,7 +18,11 @@ if (mac != '') {
 	document.getElementsByTagName('a')[1].href += '?mac=' + mac;
 }
 
-//var customer = getCookie('order');
-//if (customer != 'new') {
-//	document.getElementsByTagName('a')[1].style.display = 'none';
-//}
+var customer = getCookie('order');
+if (customer != 'new') {
+	document.getElementsByTagName('a')[1].style.display = 'none';
+}
+
+if (mac == '') {
+	document.getElementsByTagName('a')[1].style.display = 'inline';
+}
